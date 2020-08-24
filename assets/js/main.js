51f192ac59cfcb93bf7dac35a3a8d0e3ee0e9c4b
cipkorea <cipkorea@kr.ibm.com>
2020-08-24 14:08:34 +0900

    scroll nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
-$('.index .nav-a').click(function(){
-    $('.index .nav-a').removeClass('active');
+$(window).scroll(function() {
+    let fromTop = window.scrollY +1;
+  
+    $('.nav-a').each(function(i, link) {
+      let section = document.querySelector(link.hash);
+      if (
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight > fromTop
+      ) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active");
+      }
+    });
+    
+    }).scroll();
+
+$('.nav-a').click(function(){
+    $('.nav-a').removeClass('active');
     $(this).addClass('active');
 })
